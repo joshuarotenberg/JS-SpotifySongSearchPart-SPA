97839fce0981f7487fbc86733686b273310580b6
Joshua Rotenberg <joshua.rotenberg@gmail.com>
2020-11-10 09:27:44 -0500

    SPA working with search feature

--- src/controllers/searchPageController.js
+++ src/controllers/searchPageController.js
@@ -2,15 +2,68 @@ import axios from "axios";
 import Handlebars from "handlebars";
 
 export default function searchPageController() {
+  axios
+  .get("templates/searchPage.hbs")
+  .then((searchPageResponse) => {
     axios
-    .get('templates/searchPage.hbs')
-    .then((searchPageResponse) => {
-        axios
-        .get('templates/searchResult.hbs')
-        .then((searchResultResponse) => {
-            return render(searchPageResponse.data, searchResultResponse.data);
-          });
+    .get("templates/searchResult.hbs")
+    .then((searchResultResponse) => {
+      return render(searchPageResponse.data, searchResultResponse.data);
     });
+  });
 
-    
+  function render(searchPageTemplateHtml, searchResultTemplateHtml) {
+    const searchPageTemplateFunc = Handlebars.compile(searchPageTemplateHtml);
+    const searchResultTemplateFunc = Handlebars.compile(searchResultTemplateHtml);
+
+    document
+    .getElementById("root")
+    .innerHTML = searchPageTemplateFunc();
+
+    // Container that completed Handlebars template HTML will go into
+    const songCard = document.getElementById("song-index");
+
+    document
+    .getElementById("find-song")
+    .addEventListener("submit", function(e) {
+        e.preventDefault();
+        console.log("search song form submission");
+
+        const authToken = window.localStorage.getItem("authToken");
+
+
+        // If auth token is not saved to localStorage, don't allow Axios request
+        if (!authToken) {
+          return;
+        }
+
+        axios
+        .get("https://api.spotify.com/v1/search", {
+          params: {
+            q: document.getElementById("song-query").value,
+            type: "track"
+          },
+          headers: {
+            "Authorization": "Bearer " + authToken
+          }
+        })
+        .then(function(response) {
+        var songs = response.data.tracks.items;
+
+            songs.forEach(function(song) {
+                songCard.innerHTML += searchResultTemplateFunc({
+                    song_name: song.name,
+                    artist: song.artists[0].name,
+                    album: song.album.name,
+                    preview_url: song.preview_url,
+                    album_image: song.album.images[1].url,   
+                    popularity: song.popularity  
+                });
+            })
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    });
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-const greeting = "Hello World!";
+import searchPageController from "./controllers/searchPageController";
 
-const sayHello = () => {
-    console.log(greeting);
+const routes = {
+  "/search": searchPageController
+};
+
+function setRoute() {
+  // Before activating the routing, let's check for Spotify access token
+  var tokenRegex = /access_token=(.+)&token_type=Bearer/;
+  console.log(tokenRegex);
+  var hashMatches = window.location.hash.match(tokenRegex);
+  console.log(hashMatches);
+
+  if (hashMatches && hashMatches[1]) {
+    // Set the token to localStorage to use later
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+    window.localStorage.setItem("authToken", hashMatches[1]);
+  }
+
+  const currentRoute = window
+  .location
+  .hash
+  .split("#")[1];
+
+  for (let route in routes) {
+    if (currentRoute === route) {
+      return routes[route]();
+    }
+  }
+
+  // If nothing matches, trigger the search route
+  window.location.hash = "/search";
 }
 
-sayHello();
+// Trigger the function on page load
+document.addEventListener("DOMContentLoaded", setRoute);
+// Trigger the function when the hash changes
+window.addEventListener("hashchange", setRoute);
